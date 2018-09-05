37376a0e2d99b3b50c536e2a75b5704b502b93d0
skysf <35585005+skysf@users.noreply.github.com>
2018-09-05 14:06:14 -0700

    Updaed JS file
    added Hello Word for the home page for Lab2

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+/*
+Name: TIANYU(SKY) LU
+Student ID: @00300456
+Assignment: No.2
+*/
+
+window.onload = writeMessage;//When pages loads, run function
+
+function writeMessage() {
+    
+    document.getElementById("helloMessage").innerHTML = "Hello, worlds!";
+}
