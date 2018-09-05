87c4c0531dbb5333094c949555ff64156539b229
skysf <35585005+skysf@users.noreply.github.com>
2018-09-05 15:03:49 -0700

    Updated for Lab3
    added button, but alert window does not pop up.

--- JS/script.js
+++ JS/script.js
@@ -3,10 +3,31 @@ Name: TIANYU(SKY) LU
 Student ID: @00300456
 Assignment: No.2
 */
-
+window.onload = initButtons; //Add buttons
 window.onload = writeMessage;//When pages loads, run function
 
 function writeMessage() {
-    
+    //Display textword on index.html
     document.getElementById("helloMessage").innerHTML = "Hello, worlds!";
+}
+
+function initButtons() {
+	document.getElementById("Linkedin").onclick = showSomething;
+	document.getElementById("AppStore").onclick = showSomething;
+	document.getElementById("Apartment").onclick = showSomething;
+}
+
+function showSomething() {
+	switch(this.id) {
+		case "Linkedin":
+			alert("https://www.linkedin.com/in/lu-sky-a3850138/");
+			break;
+		case "AppStore":
+			alert("Ask not what your country can do for you...");
+			break;
+		case "Apartment":
+			alert("I had my own apartment before!");
+			break;
+		default:
+	}
 }
